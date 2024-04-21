5ca769c742a89a52164859d04203cb5198c0fa22
Taishi Naka <taishi.develop@gmail.com>
2024-04-21 23:47:56 +0900

    add git switch with fzf

--- nix/git.nix
+++ nix/git.nix
@@ -8,6 +8,7 @@
         cm = "commit -m";
         df = "diff";
         br = "branch";
+        lgn = "log --graph --decorate --abbrev-commit --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset)%C(yellow)%d%C(reset)\n  %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
     };
 
     extraConfig = {
